edeb8a4a6721828e8f631827f16649c99a3df06b
Joppaa-Main <joppaaplc@gmail.com>
2024-07-27 23:53:49 +0100

    done....

--- src/app/components/nav.jsx
+++ src/app/components/nav.jsx
@@ -12,8 +12,8 @@ export default function Nav() {
   const reserveLink = "https://www.pronto-food-online.com/ordering/restaurant/menu/reservation?restaurant_uid=175b2cdb-5680-4673-b025-59f548105410&reservation=true&glfa_cid=1277109742.1603227119&glfa_t=1603227121089"
   const menuItems = [
     ['Menu', '/menu'],
-    ['About us - tbc', '/'],
-    ['Call us', 'tel:01614255080'],
+   
+    ['Call us', 'tel:01614255080'], ['About HQ', '/'],
     // ['Order online', orderlink],
     // ['Reserve a table', reserveLink],
   ]
@@ -43,16 +43,18 @@ export default function Nav() {
       </div>
       {navbar && <div id='menu' className=' absolute top-[72px] left-3 rounded-md bg-black  w-screen shadow-sm flex max-w-[660px] h-[460px] flex-col items-start z-20'>
 
-        <div className='p-4 flex flex-col gap-4 font-bold justify-center '>
-          <h1 className='px-2 text-sm mb-3'> Headquater </h1>
+        <div className='p-4 mt-4  flex flex-col gap-3 font-bold justify-center '>
+          
           {menuItems.map(([text, url], i) => (
-            <Link onClick={handleNav} key={i} href={url} className='hover:text-green-400 focus:underline underline-offset-8 px-2 text-lg '> {text}</Link>))}
+            <Link onClick={handleNav} key={i} href={url} className='hover:text-green-400 focus:underline underline-offset-8 px-2 text-lg uppercase '> {text}</Link>))}
           <Link onClick={handleNav}  href={orderlink} target="_blank" className='px-4 py-2 hover:font-semibold bg-red-600 rounded-sm  uppercase '   > Order online</Link>
           <Link onClick={handleNav}  href={reserveLink} target="_blank" className=' hover:font-semibold px-4 py-2 mt-1 bg-green-500 rounded-md uppercase '   > Reserve a table</Link>
-          <div className='flex flex-row text-xl p-4 gap-4 mt-5'>
-          <Link onClick={handleNav}  href={"https://www.facebook.com/hqmanchester"}target="_blank" ><FaFacebook /></Link><Link onClick={handleNav}  href={"https://www.instagram.com/hqmanchester/"} target="_blank"><FaInstagram /></Link>
+          <div className='flex flex-row text-2xl p-4 gap-4 '>
+          <Link onClick={handleNav} className=''  href={"https://www.facebook.com/hqmanchester"}target="_blank" ><FaFacebook /></Link><Link onClick={handleNav}  href={"https://www.instagram.com/hqmanchester/"} target="_blank"><FaInstagram /></Link>
             <Link onClick={handleNav}  href={"https://www.tiktok.com/@hqmanchester/"} target="_blank"><FaTiktok /></Link>
           </div>
+          <span className='px-2 text-sm  gap-2 flex flex-col'> <p className='font-light'>Opening Hours</p> <p>Monday - Sunday</p><p>4pm - 11pm</p>
+          </span>
         </div>
         <span onClick={handleNav} className='text-white absolute text-xs  cursor-pointer top-5 right-12'>X close</span>
       </div>}
