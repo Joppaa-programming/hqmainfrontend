b9974434874c9b201f7827cd6cf96248ce1005b8
Joppaa-Main <joppaaplc@gmail.com>
2024-09-10 02:21:13 +0100

    order sort

--- src/app/components/videoPlayer.jsx
+++ src/app/components/videoPlayer.jsx
@@ -3,18 +3,19 @@ import React from 'react'
 export default function VideoPlayer() {
     return (
         <div className=" mt-[-10px] md:mt-[-60px] flex gap-4 flex-row  overflow-x-scroll overflow-y-hidden px-4">
-            <video width="320px" height="720px" preload="auto" controls >
+             <video width="320px" height="720px" preload="auto" controls >
+                <source src="/video.webm" />
+                videoPlayer</video> <video width="320px" height="720px" preload="auto" controls >
                 <source src="/hqvid.mp4" />
                 videoPlayer</video>
             <video width="320px" height="720px" preload="auto" controls >
                 <source src="/hqad.mp4" />
                 videoPlayer</video>
-                <video width="320px" height="720px" preload="auto" controls >
-                <source src="/video.webm" />
-                videoPlayer</video>
+              
             <video width="320px" height="720px" preload="auto" controls >
                 <source src="/restaurant.mp4" />
                 videoPlayer</video>
+                Video could not load, use different browser
 
         </div>
     )
