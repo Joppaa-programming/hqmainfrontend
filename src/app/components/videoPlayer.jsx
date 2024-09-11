28edc40bf7979b53568bc363fd7d5f641d7aa786
Joppaa-Main <joppaaplc@gmail.com>
2024-09-11 17:57:11 +0100

    margin

--- src/app/components/videoPlayer.jsx
+++ src/app/components/videoPlayer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function VideoPlayer() {
     return (
-        <div className=" mt-[-10px] md:mt-[-60px] flex gap-4 flex-row  overflow-x-scroll overflow-y-hidden px-4">
+        <div className=" mt-[5px] md:mt-[-60px] flex gap-4 flex-row  overflow-x-scroll overflow-y-hidden px-4">
              <video width="320px" height="720px" preload="auto" poster={"/foodvid.png"}  controls >
                 <source src="/video.webm" />
                 videoPlayer</video> <video width="320px" height="720px" preload="auto" poster={"/carvid.png"}  controls >
