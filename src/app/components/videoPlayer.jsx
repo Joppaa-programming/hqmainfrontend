42df8cd49eca6b8533171a99e3d8ddfce297cd27
Joppaa-Main <joppaaplc@gmail.com>
2024-09-16 01:45:49 +0100

    cantrols

--- src/app/components/videoPlayer.jsx
+++ src/app/components/videoPlayer.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 export default function VideoPlayer() {
     return (
         <div className=" mt-[5px] md:mt-[-60px] flex gap-4 flex-row  overflow-x-scroll overflow-y-hidden px-4">
-             <video width="320px" height="720px" preload="auto" poster={"/foodvid.png"}   >
+             <video width="320px" height="720px" preload="auto" poster={"/foodvid.png"}  controls >
                 <source src="/video.webm" />
-                videoPlayer</video> <video width="320px" height="720px" preload="auto" poster={"/carvid.png"}   >
+                videoPlayer</video> <video width="320px" height="720px" preload="auto" poster={"/carvid.png"}  controls >
                 <source src="/hqvid.mp4" />
                 videoPlayer</video>
-            <video width="320px" height="720px" poster={"/visitvid.png"} preload="auto"  >
+            <video width="320px" height="720px" poster={"/visitvid.png"} preload="auto" controls >
                 <source src="/hqad.mp4" />
                 videoPlayer</video>
               
-            <video width="320px" height="720px" preload="auto" poster={"/shopvid.png"}   >
+            <video width="320px" height="720px" preload="auto" poster={"/shopvid.png"}  controls >
                 <source src="/restaurant.mp4" />
                 videoPlayer</video>
           
